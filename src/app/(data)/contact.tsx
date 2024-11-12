f025512f8313d47a9c83d9e5b851e9cb7c3fc05b
Omar Sadek <sadekm.omar@gmail.com>
2024-11-12 12:16:21 +0200

    feat: created data objects for menuPages and contactInfo

---
+++ src/app/(data)/contact.tsx
@@ -0,0 +1,6 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export const contacts = [
+  { name: "Github", url: "https://github.com/sadekomar", icon: <FaGithub /> },
+  { name: "LinkedIn", url: "https://www.linkedin.com/in/sadekomar/", icon: <FaLinkedin /> },
+];
---
+++ src/app/(data)/menuPages.ts
@@ -0,0 +1,7 @@
+export const menuPages = [
+  { name: "Home", slug: "/" },
+  { name: "About", slug: "/about" },
+  { name: "Work", slug: "/work" },
+  { name: "Contact", slug: "/contact" },
+  { name: "Blog", slug: "/blog" },
+];
