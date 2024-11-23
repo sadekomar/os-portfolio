7f86ef8ccfec7c90c2c631d320b23dfad53c28fa
Omar Sadek <sadekm.omar@gmail.com>
2024-11-23 16:35:40 +0200

    feat: improved footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,47 +6,51 @@ import Link from "next/link";
 export function Footer() {
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 rounded-t-2xl bg-gray-200 px-20 py-20 md:grid-cols-[3fr_1fr_1fr]">
-        <div className="flex flex-col gap-2">
-          <h3 className="font-bold">Omar Sadek</h3>
-          <p className="max-w-[60ch]">
-            Full-Stack Software Engineer interested in design-led UX/UI, performant databases, and
-            scalable application architectures. I thrive on combining technical expertise with
-            creative design to create technically robust yet engaging products.
-          </p>
-        </div>
+      <div className="rounded-t-2xl bg-gray-100 px-20 py-20">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-[3fr_1fr_1fr]">
+          <div className="flex flex-col gap-2">
+            <h3 className="font-bold">Omar Sadek</h3>
+            <p className="max-w-[60ch]">
+              Full-Stack Software Engineer interested in design-led UX/UI, performant databases, and
+              scalable application architectures. I thrive on combining technical expertise with
+              creative design to create technically robust yet engaging products.
+            </p>
+          </div>
 
-        <div className="flex flex-col gap-2">
-          <h3 className="font-bold">Navigate</h3>
-          {menuPages.map((page) => (
-            <Link href={page.slug} className="hover:underline" key={page.slug}>
-              {page.name}
-            </Link>
-          ))}
-        </div>
+          <div className="flex flex-col gap-2">
+            <h3 className="font-bold">Navigate</h3>
+            {menuPages.map((page) => (
+              <Link href={page.slug} className="hover:underline" key={page.slug}>
+                {page.name}
+              </Link>
+            ))}
+          </div>
 
-        <div className="flex flex-col gap-2">
-          <h3 className="font-bold">Contact</h3>
-          {contacts.map((contact, index) => (
+          <div className="flex flex-col gap-2">
+            <h3 className="font-bold">Contact</h3>
+            {contacts.map((contact, index) => (
+              <a
+                href={contact.url}
+                className="flex items-center gap-2 hover:underline hover:underline-offset-2"
+                target="_blank"
+                key={index}
+                rel="noopener noreferrer"
+              >
+                {contact.icon}
+                {contact.name}
+              </a>
+            ))}
             <a
-              href={contact.url}
-              className="flex items-center gap-2 hover:underline hover:underline-offset-2"
-              target="_blank"
-              key={index}
-              rel="noopener noreferrer"
+              href="mailto:sadekm.omar@gmail.com"
+              className="flex items-center gap-2 text-sm hover:underline hover:underline-offset-2"
             >
-              {contact.icon}
-              {contact.name}
+              <Inbox className="h-4 w-4" />
+              sadekm.omar@gmail.com
             </a>
-          ))}
-          <a
-            href="mailto:sadekm.omar@gmail.com"
-            className="flex items-center gap-2 text-sm hover:underline hover:underline-offset-2"
-          >
-            <Inbox className="h-4 w-4" />
-            sadekm.omar@gmail.com
-          </a>
+          </div>
         </div>
+        <div className="my-6 h-px w-full bg-gray-300"></div>
+        <div className="text-sm">Copyright © 2024 Omar Sadek. All Rights Reserved.</div>
       </div>
     </>
   );
