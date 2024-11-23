bca0b566f81116ae826a55834655477965c541c1
Omar Sadek <sadekm.omar@gmail.com>
2024-11-23 23:44:27 +0200

    feat: improved home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,104 +5,79 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="mx-20 my-40 flex flex-col items-start justify-center gap-4">
-        <h2 className="max-w-md font-sans text-4xl font-medium">
-          I’m a Full-Stack Software Engineer, UI/UX Designer, and Founder
+      <div className="mx-4 mb-10 flex flex-col items-start justify-center gap-4 md:mx-20">
+        <h2 className="max-w-md text-4xl font-medium">
+          I’m a Full-Stack Software Engineer, UI/UX Designer, and Founder.
         </h2>
-        <p className="max-w-md text-2xl">
+        <p className="max-w-md text-2xl font-medium">
           I build web apps that aren’t just delightful and intuitive, but also also technically
           robust.
         </p>
-        <Link
-          className="flex h-10 items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm transition-colors hover:border-transparent hover:bg-[rgb(242,242,242)] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] sm:h-12 sm:min-w-44 sm:px-5 sm:text-base"
-          href="/work"
-        >
-          View work
-        </Link>
-        <a
-          href="mailto:sadekm.omar@gmail.com"
-          className="flex h-10 w-fit items-center gap-2 rounded-xl bg-[#E4E4E4] px-4"
-        >
-          Contact
-        </a>
-        <a
-          className="gap flex h-10 items-center gap-2 rounded-xl border-2 border-solid border-gray-700 px-2 font-medium transition-colors hover:bg-gray-700 hover:text-white"
-          href="./resume.pdf"
-          download={"resume-omar-sadek.pdf"}
-        >
-          <Download height={16} width={16} />
-          Resume
-        </a>
+        <div className="flex gap-2">
+          <a
+            className="gap flex h-10 items-center gap-2 rounded-3xl border-2 border-solid border-gray-700 px-4 font-medium transition-colors hover:bg-gray-700 hover:text-white"
+            href="./resume.pdf"
+            download={"resume-omar-sadek.pdf"}
+          >
+            <Download height={16} width={16} />
+            Resume
+          </a>
+          <a
+            href="mailto:sadekm.omar@gmail.com"
+            className="flex h-10 w-fit items-center gap-2 rounded-3xl bg-[#E4E4E4] px-4 transition-colors hover:bg-gray-300"
+          >
+            <Inbox height={16} width={16} />
+            Contact
+          </a>
+        </div>
       </div>
 
-      <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-        <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-            <li className="mb-2">
-              Get started by editing{" "}
-              <code className="rounded bg-black/[.05] px-1 py-0.5 font-semibold dark:bg-white/[.06]">
-                src/app/page.tsx
-              </code>
-              .
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
-
-          <div className="flex flex-col items-center gap-4 sm:flex-row">
-            <a
-              className="flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent bg-foreground px-4 text-sm text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] sm:h-12 sm:px-5 sm:text-base"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
+      <h2 className="mx-4 mb-4 text-2xl font-medium underline-offset-4 group-hover:underline">
+        Work
+      </h2>
+      <div className="mx-4 mb-10 grid gap-4 md:grid-cols-3">
+        <Link
+          href={"/work/loom-cairo"}
+          className="group flex flex-col items-start gap-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-950"
+        >
+          <img src="./loom-thumbnail.png" alt="" />
+          <div>
+            <h3 className="text-xl font-medium underline-offset-4 group-hover:underline">
+              Loom Cairo
+            </h3>
+            <p className="font-medium tracking-[-0.02em]">
+              A fashion search engine that aggregates more than 300 local brands.
+            </p>
           </div>
-        </main>
-        <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image aria-hidden src="/file.svg" alt="File icon" width={16} height={16} />
-            Learn
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
-            Examples
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
-            Go to nextjs.org →
-          </a>
-        </footer>
+        </Link>
+        <Link
+          href={"/work/activity-management-platform"}
+          className="group flex flex-col items-start gap-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-950"
+        >
+          <img src="./un-thumbnail.png" alt="" />
+          <div>
+            <h3 className="text-xl font-medium underline-offset-4 group-hover:underline">
+              UN Activity Management Platform
+            </h3>
+            <p className="font-medium tracking-[-0.02em]">
+              A platform to streamline event tracking for a UN agency.
+            </p>
+          </div>
+        </Link>
+        <Link
+          href={"/work/little-lads"}
+          className="group flex flex-col items-start gap-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-950"
+        >
+          <img src="./lads-thumbnail.png" alt="" />
+          <div>
+            <h3 className="text-xl font-medium underline-offset-4 group-hover:underline">
+              Little Lads
+            </h3>
+            <p className="font-medium tracking-[-0.02em]">
+              A fashion search engine that aggregates more than 300 local brands.
+            </p>
+          </div>
+        </Link>
       </div>
     </>
   );
